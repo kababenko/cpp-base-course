d49129ca3373556251468a1a9ff0ebf072cae60e
kababenko <144918492+kababenko@users.noreply.github.com>
2023-12-17 16:58:48 +0300

    1

---
+++ Задача6/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <array>
+
+int max(int a, int b) {
+    if (a > b) {
+        return a;
+    }
+    else {
+        return b;
+    }
+}
+
+int function(int prices[]) {
+    int purchase = prices[0];
+    int  profit = 0;
+        for (int i = 1; i < sizeof(prices); i++) {
+            if (prices[i] < purchase) {
+                purchase = prices[i];
+
+            }
+            else profit = max(profit, prices[i] - purchase);
+        }
+    return profit;
+}
+
+int main(){
+    int n;
+    int prices[8]{ 3,3,5,0,0,3,1,4 };
+    std::cout << function(prices);
+}
